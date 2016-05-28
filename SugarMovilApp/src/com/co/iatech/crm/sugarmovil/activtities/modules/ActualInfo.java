9adc2af94eabcbd79f6faa63d32c7e69f82294bf
Hector Sanchez <hsanchezg7@gmail.com>
2016-05-28 11:25:35 -0500

    no message

--- SugarMovilApp/src/com/co/iatech/crm/sugarmovil/activtities/modules/ActualInfo.java
+++ SugarMovilApp/src/com/co/iatech/crm/sugarmovil/activtities/modules/ActualInfo.java
@@ -1,19 +1,53 @@
 package com.co.iatech.crm.sugarmovil.activtities.modules;
 
 public class ActualInfo{
-	private GenericModuleBean actualParentModule;
-	private GenericModuleBean actualPrincipalModule;
+	private Modules actualParentModule;
+	private String actualParentId;
+	private Modules actualPrincipalModule;
+	private String actualPrincipalId;
 	
 	
 	public ActualInfo(){
-		actualParentModule = new GenericModuleBean();
-		actualPrincipalModule = new GenericModuleBean();
+		actualParentModule = Modules.PREVIOUS_UI;
+		actualPrincipalModule = Modules.PREVIOUS_UI;
 	}
 	
-	public ActualInfo(GenericModuleBean actualParentModule){
+	public ActualInfo(Modules actualParentModule,String actualParentId ){
 		this.actualParentModule = actualParentModule;
-		actualPrincipalModule = new GenericModuleBean();
+		this.actualParentId  = actualParentId;
 	}
 
+	public Modules getActualParentModule() {
+		return actualParentModule;
+	}
+
+	public void setActualParentModule(Modules actualParentModule) {
+		this.actualParentModule = actualParentModule;
+	}
+
+	public String getActualParentId() {
+		return actualParentId;
+	}
+
+	public void setActualParentId(String actualParentId) {
+		this.actualParentId = actualParentId;
+	}
+
+	public Modules getActualPrincipalModule() {
+		return actualPrincipalModule;
+	}
+
+	public void setActualPrincipalModule(Modules actualPrincipalModule) {
+		this.actualPrincipalModule = actualPrincipalModule;
+	}
+
+	public String getActualPrincipalId() {
+		return actualPrincipalId;
+	}
+
+	public void setActualPrincipalId(String actualPrincipalId) {
+		this.actualPrincipalId = actualPrincipalId;
+	}
+	
 	
 }
